d5ca79b89ed03b773aa308c0798153d0c2da5074
Suleman Soren <suleman@cronj.com>
2023-08-27 12:22:02 +0530

    Counter implemented using class component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Card from "../Components/Cards/Card";
-import './App.css';
+import Counter from "../Pages/Counter";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Card
-        title="Lizard"
-        description="Lizards are a widespread group of squamate reptiles, with over 7,000 species, ranging across all continents except Antarctica, as well as most oceanic island chains."
-        imageSrc="https://post.healthline.com/wp-content/uploads/2021/06/lizard-iguana-732x549-thumbnail.jpg"
-      />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="counter" element={<Counter />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Counter/index.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component<CounterProps, CounterState> {
+  constructor(props: CounterProps) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Counter: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
+interface CounterProps {}
+
+interface CounterState {
+  count: number;
+}
