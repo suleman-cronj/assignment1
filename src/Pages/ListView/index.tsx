ae2e26068174db2f4954e65eedeca13e96c69d77
Suleman Soren <suleman@cronj.com>
2023-08-28 11:14:11 +0530

    Added form to list view page

--- src/Pages/ListView/index.tsx
+++ src/Pages/ListView/index.tsx
@@ -1,13 +1,17 @@
 import { Component } from "react";
 import Card from "../../Components/Cards/Card";
-import { Grid } from "@mui/material";
+import { Grid, TextField, Button } from "@mui/material";
 import axios from "axios";
-
+import "./index.css";
 class ListView extends Component<ListViewProps, ListViewState> {
   constructor(props: ListViewProps) {
     super(props);
     this.state = {
       data: [],
+      firstName: "",
+      lastName: "",
+      email: "",
+      avatar: "",
     };
   }
 
@@ -18,12 +22,83 @@ class ListView extends Component<ListViewProps, ListViewState> {
       .catch((error) => console.error("Error fetching data:", error));
   }
 
+  handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const newData = [...this.state.data];
+    const newElement = {
+      id: newData.length + 1,
+      avatar: this.state.avatar,
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      email: this.state.email,
+    };
+    newData.push(newElement);
+    this.setState({ data: newData });
+  };
+
+  handleFirstNameChange = (e: { target: { value: any } }) => {
+    this.setState({ firstName: e.target.value });
+  };
+
+  handleLastNameChange = (e: { target: { value: any } }) => {
+    this.setState({ lastName: e.target.value });
+  };
+
+  handleEmailChange = (e: { target: { value: any } }) => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleAvatarChange = (e: { target: { value: any } }) => {
+    this.setState({ avatar: e.target.value });
+  };
+
   render() {
     const { data } = this.state;
-    console.log(data);
     return (
       <div>
-        <h2>Data from API</h2>
+        <h2>Profile List</h2>
+        <form onSubmit={this.handleSubmit} className="form-container">
+          <TextField
+            label="First Name"
+            variant="outlined"
+            value={this.state.firstName}
+            onChange={this.handleFirstNameChange}
+            margin="normal"
+            className="form-element"
+          />
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            value={this.state.lastName}
+            onChange={this.handleLastNameChange}
+            margin="normal"
+            className="form-element"
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+            margin="normal"
+            className="form-element"
+          />
+          <TextField
+            label="Avatar"
+            variant="outlined"
+            value={this.state.avatar}
+            onChange={this.handleAvatarChange}
+            margin="normal"
+            className="form-element"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className="submit-button"
+          >
+            Submit
+          </Button>
+        </form>
         <Grid container spacing={2}>
           {data.map((item) => (
             <Grid item xs={12} sm={6} md={4} key={item.id}>
@@ -54,4 +129,8 @@ interface DataItem {
 
 interface ListViewState {
   data: DataItem[];
+  firstName: string;
+  lastName: string;
+  email: string;
+  avatar: string;
 }
